bf632ffea2cade8246180f63a9d2de1334e81a01
shubhadanikam <108353116+shubhadanikam@users.noreply.github.com>
2023-05-27 23:50:48 +0530

    Add files via upload

---
+++ SetNavigation.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SetNavigation : MonoBehaviour
+{
+    [SerializeField]
+    private TMP_Dropdown navigationTargetDropDown;
+    [SerializeField]
+    private List<Target> navigationTargetObjects = new List<Target>();
+
+    private NavMeshPath path;// current calculated path
+    private LineRenderer line;// LineRenderer to display path
+    private Vector3 targetPosition = Vector3.zero;  // current target position
+
+    private bool lineToggle = false; // Start is called before the first frame update
+    
+   private void Start()
+    {
+        path = new NavMeshPath();
+        line = transform.GetComponent<LineRenderer>();
+        line.enabled = lineToggle;
+    }
+
+     // Update is called once per frame
+    private void Update()
+    {
+        if(lineToggle && targetPosition!= Vector3.zero)
+        {
+            NavMesh.CalculatePath(transform.position, targetPosition, NavMesh.AllAreas, path);
+            line.positionCount = path.corners.Length;
+            line.SetPositions(path.corners);
+        }
+    }
+
+    public void SetCurrentNavigationTarget(int selectedValue)
+    {
+        targetPosition = Vector3.zero;
+        string selectedText = navigationTargetDropDown.options[selectedValue].text;
+        Target currentTarget = navigationTargetObjects.Find(x => x.Name.Equals(selectedText));
+        if(currentTarget != null)
+        {
+            targetPosition = currentTarget.PositionObject.transform.position;
+        }
+    }
+
+    public void ToogleVisibility()
+    {
+        lineToggle = !lineToggle;
+        line.enabled = !lineToggle;
+    }
+}
